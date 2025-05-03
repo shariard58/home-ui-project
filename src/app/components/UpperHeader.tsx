19e6a3d0f31713614623d7826b1fb9d658270318
shariard58 <shariard58@gmail.com>
2025-05-03 12:27:08 +0600

    added

--- src/app/components/UpperHeader.tsx
+++ src/app/components/UpperHeader.tsx
@@ -1,6 +1,6 @@
 export default function UpperHeader() {
   return (
-    <div className='bg-gradient-to-r from-[#FC004E] to-[#10CBE0] py-2'>
+    <div className='py-2 bg-[linear-gradient(to_right,var(--primary),var(--secondary))] font-figs'>
       <p>This is upper Header</p>
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Figtree, Urbanist } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const urbanist = Urbanist({
+  variable: '--font-urbanist',
   subsets: ['latin'],
+  display: 'swap',
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const figtree = Figtree({
+  variable: '--font-figtree',
   subsets: ['latin'],
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -24,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${urbanist.variable} ${figtree.variable} font-sans antialiased`}>
+        {children}
+      </body>
     </html>
   )
 }
