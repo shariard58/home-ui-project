82862e4381c439e2cf5d1653fa1fae3a5db367a3
shariard58 <shariard58@gmail.com>
2025-05-03 17:40:27 +0600

    changes in upper Header

--- src/app/components/UpperHeader.tsx
+++ src/app/components/UpperHeader.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 
 export default function UpperHeader() {
   return (
-    <div className='py-2 bg-gradient-to-r from-[#FC004E] to-[#00e7f9] font-figs flex justify-center items-center px-8 md:px-0 sticky top-0 z-40'>
+    <div className='py-2 bg-gradient-to-r from-[#FC004E] to-[#00e7f9] font-figs flex justify-center items-center px-6 md:px-0 sticky top-0 z-40'>
       <div className='flex gap-1'>
         <Image
           src='/images/rocket-icon.png'
           alt='Rocket icon'
           width={23}
           height={22}
-          className='h-3 w-3 md:h-8 md:w-8'
+          className='h-4 w-4 md:h-8 md:w-8'
         />
         <p className='text-[16px] md:text-[22px] font-bold text-[#00e7f9 '>
           FRESH BEGINNINGS SALE:
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import Header from './components/Header'
-import Hero from './components/hero/Hero'
 import UpperHeader from './components/UpperHeader'
 
 export default function Home() {
   return (
     <div className='min-h-screen flex flex-col'>
       <UpperHeader />
-      <div className='bg-[#010101] flex-1'>
+      {/* <div className='bg-[#010101] flex-1'>
         <div className='max-w-6xl mx-auto px-4'>
           <Header />
           <Hero />
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
