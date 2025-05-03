285a6ffaef9ffa1eb58cbf4571239b24d4ae07bd
shariard58 <shariard58@gmail.com>
2025-05-03 11:45:36 +0600

    gradiant colro

--- src/app/components/UpperHeader.tsx
+++ src/app/components/UpperHeader.tsx
@@ -1,6 +1,6 @@
 export default function UpperHeader() {
   return (
-    <div className='border border-black py-2'>
+    <div className='bg-gradient-to-r from-[#FC004E] to-[#10CBE0] py-2'>
       <p>This is upper Header</p>
     </div>
   )
