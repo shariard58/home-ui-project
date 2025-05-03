6e508e81a2ee22cf270725a8713dc90b67886850
shariard58 <shariard58@gmail.com>
2025-05-03 17:50:30 +0600

    fix Header section responsivness fixed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,26 @@
 import Image from 'next/image'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function Header() {
   return (
-    <div className='flex justify-between mt-9 max-w-5xl'>
+    <div className='flex justify-between mt-4 md:mt-9 max-w-5xl'>
       <Image
         src='/images/logo.png'
         alt='Company Logo'
         width={174}
         height={74}
         sizes='(max-width: 640px) 120px, (max-width: 1024px) 150px, 174px'
-        className='w-[120px] h-auto sm:w-[150px] lg:w-[174px]'
+        className='w-[120px] h-auto sm:w-[150px] lg:w-[174px] ml-28 md:ml-0'
       />
-      <div className='flex gap-5'>
-        <a href='#' className='text-[18px] font-semibold cursor-pointer text-[#A9A9A9]'>
+
+      <div className='flex  md:hidden'>
+        <GiHamburgerMenu color='white' size={24} className='cursor-pointer' />
+      </div>
+      <div className='md:flex gap-5 hidden '>
+        <a href='#' className='font-figs text-[18px] font-semibold cursor-pointer text-[#A9A9A9]'>
           About us
         </a>
-        <a href='#' className='text-[18px] font-semibold cursor-pointer text-[#A9A9A9]'>
+        <a href='#' className='font-figs text-[18px] font-semibold cursor-pointer text-[#A9A9A9]'>
           Contact
         </a>
       </div>
